4d2b77c0bb4b7fd1037e3936f33b052a4d6756d0
kamruzzaman-insights <kamruzzaman.insights@gmail.com>
2025-11-22 12:09:02 +0600

    learning  dynamic props

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,34 @@ function App() {
       <Developer name="nahid" tech="PCB Design"></Developer>
       <Developer name="raihan" tech="JavaScript"></Developer>
       <Developer name="zaman" tech="python"></Developer>
+      <Player name="hasib" runs="5000"></Player>
+      <Player name="mushfiq" runs="5000"></Player>
+      <Salami event="roja" hadia="1000"></Salami>
+      <Salami event="graduation" hadia="2000"></Salami>
     </>
   )
 }
 
-function Developer(hh) {
-  console.log(hh);
+function Salami({event, hadia}) {
+  return(
+    <div className='student'>
+      <p>Salami for id: {event}</p>
+      <p>Amount: {hadia}</p>
+    </div>
+  )
+}
+
+function Player({name, runs}) {
+  return(
+    <div className='student'>
+      <h3>Name: {name} </h3>
+      <p>Runs: {runs} </p>
+    </div>
+  )
+}
+
+function Developer(props) {
+  console.log(props);
   return(
     <div style={{
       border: '2px solid red',
@@ -26,8 +48,8 @@ function Developer(hh) {
       padding: '5px 10px',
       margin: '5px',
     }}>
-      <h3>Developer: {hh.name} </h3>
-      <p>Technology: {hh.tech} </p>
+      <h3>Developer: {props.name} </h3>
+      <p>Technology: {props.tech} </p>
     </div>
   )
 }
